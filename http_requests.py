58a75924ddaa4775922f10731c99f809b46b2b73
Rinat Sarmuldin <ura07srr@gmail.com>
2024-01-11 21:55:05 +0500

    Fix  http_requests

--- http_requests.py
+++ http_requests.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'https://www.google.com/afadf'
+url = 'https://www.google.com'
 response = requests.get(url)
 print(f'Request to {url}. Status code is {response.status_code}.')
-print(response.text)
+# print(response.text)
