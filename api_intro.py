995fb3de08b5a50dbfe9351d517cc60c3e41e941
Rinat Sarmuldin <ura07srr@gmail.com>
2024-01-12 14:03:06 +0500

    earthquake_app(example_Api)

--- api_intro.py
+++ api_intro.py
@@ -3,12 +3,11 @@ import requests
 url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?'
 response = requests.get(url, headers={'Accept':'application/json'}, params={
 		'format':'geojson',
-		'starttime':'2019-01-01',
-		'endtime':'2019-05-01',
+		'starttime':'2020-01-01',
+		'endtime':'2024-01-01',
 		'latitude':'51.51',
 		'longitude':'-0.12',
 		'maxradiuskm':'2000'
-
 	})
 # print(response.text)
 # print(type(response.json()))
